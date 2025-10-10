977781856b110cee7234426596e028d2219ed66c
Meherun Nesa Arpa <nesa.arpa@gmail.com>
2025-10-10 10:24:12 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DreamTradeLLC/', // 👈 this MUST match your repo name exactly
+  base: './', // temporary: load everything relative
 })
